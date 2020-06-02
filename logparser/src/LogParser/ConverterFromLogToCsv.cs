893c87a66caeca01b8814eb19e4421d7b91a2a89
Mayank <mayank.jain@prorigosoftware.com>
2020-06-02 13:23:52 +0530

    added log file parser and formatter

--- logparser/src/LogParser/ConverterFromLogToCsv.cs
+++ logparser/src/LogParser/ConverterFromLogToCsv.cs
@@ -5,15 +5,40 @@ namespace LogParser
 {
     public class ConverterFromLogToCSV
     {
-        private string[] logfiles;
+
         public void convert(Arguments args)
         {
-            GetLogFiles(args.logdir);
+            string[] logfiles = GetLogFiles(args.logdir);
+            foreach (var filename in logfiles)
+            {
+                AppendToCsv(filename, args.csv);
+            }
         }
 
-        private void GetLogFiles(string logdir)
+        private void AppendToCsv(string filename, string csvfile)
         {
-            throw new NotImplementedException();
+            var csv = new CsvFormatter();
+
+            using (var writter = new StreamWriter(csvfile))
+            {
+                using (var reader = File.OpenText(filename))
+                {
+                        var line = reader.ReadLine();
+                        var formatdata = csv.ParseLine(line);
+                }
+            }
+            // var newLine = string.Format("{0},{1}", first, second);
+            // writter.WriteLine(line)
+            //     csv.AppendLine(newLine);
+            // AddRecordToCSV(data);
+
+            // File.WriteAllText(filePath, csv.ToString());
+        }
+
+        private string[] GetLogFiles(string logdir)
+        {
+            string[] logfiles = Directory.GetFiles(logdir, "*.log", SearchOption.AllDirectories);
+            return logfiles;
         }
     }
 }
---
+++ logparser/src/LogParser/CsvData.cs
@@ -0,0 +1,21 @@
+// Assuming the data in log file are in format 
+//  <date>(month/day) <Time>(hour:minute:second) <loglevel> : <text>
+//
+using System;
+using System.IO;
+
+namespace LogParser
+{
+    public class CsvData
+    {
+        public string Level { set; get; }
+        public string Date { set; get; }
+        public string Time { set; get; }
+        public string Text { set; get; }
+
+        public string GetString()
+        {
+            return $"{Level},{Date},{Time},{Text}";
+        }
+    }
+}
