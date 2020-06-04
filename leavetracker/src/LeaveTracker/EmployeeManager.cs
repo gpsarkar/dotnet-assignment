1ffbe4086283c3e3638e7d0c022f510004604b13
Mayank <mayank.jain@prorigosoftware.com>
2020-06-04 13:55:23 +0530

    added employee manager class to manage employee

---
+++ leavetracker/src/LeaveTracker/EmployeeManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+
+namespace LeaveTracker
+{
+    public class EmployeeManager
+    {
+        public Employee E { get; set; }
+
+        public bool GetEmployeeFromCsv(int id)
+        {
+            using( var reader = File.OpenText("employee.csv"))
+            {
+                var entry = reader.ReadLine();
+                while(entry != null)
+                {
+                    var employ = entry.Split(",");
+                    if (int.Parse(employ[0]) == id )
+                    {
+                        E.ID = int.Parse(employ[0]);
+                        E.Name = employ[1];
+                        E.ManID = int.Parse(employ[2]);
+                        return true;
+                    }
+                    entry = reader.ReadLine();
+                }
+                return false;
+            }
+
+        }
+
+        public void AddEmployye()
+        {
+        }
+
+        public void login()
+        {
+            while(true)
+            {
+                Console.WriteLine("Enter the Employee ID or q to quit: ");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    Environment.Exit(0);
+                }
+                var id = int.Parse(input);
+                var success = GetEmployeeFromCsv(id);
+            }
+        }
+    }
+}
