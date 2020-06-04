8f83a7797de6c489057cdcf1c192501786163ec4
Mayank <mayank.jain@prorigosoftware.com>
2020-06-04 13:53:34 +0530

    modified leavetracker class variables

--- leavetracker/src/LeaveTracker/Leave.cs
+++ leavetracker/src/LeaveTracker/Leave.cs
@@ -6,10 +6,23 @@ namespace LeaveTracker
     {
         public int ID { get; set; }
         public string Creator { get; set; }
-        public int ID { get; set; }
-        public int ID { get; set; }
-        public int ID { get; set; }
-        public int ID { get; set; }
-        public int ID { get; set; }
+        public string Manager { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LeaveStatus Status { get; set; }
+
+        public void AddLeave()
+        {
+
+        }
+
+        public void EditLeaveStatus()
+        {
+
+        }
+
+        
     }
 }
