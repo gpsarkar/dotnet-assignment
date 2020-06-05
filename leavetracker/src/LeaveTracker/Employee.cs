e10e94008ce92c9c9b658336e941f5899c118591
Mayank <mayank.jain@prorigosoftware.com>
2020-06-05 13:53:50 +0530

    Add validate method

--- leavetracker/src/LeaveTracker/Employee.cs
+++ leavetracker/src/LeaveTracker/Employee.cs
@@ -13,5 +13,14 @@ namespace LeaveTracker
         public int ID { get; set; }
         public string Name { get; set; }
         public int ManID { get; set; }
+        
+        public bool valid()
+        {
+            if ( ID != 0 && Name != "" && ManID != 0)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
