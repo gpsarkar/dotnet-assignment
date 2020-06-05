ede065fd847994d04499ae009ef1ecbec5e164a8
Mayank <mayank.jain@prorigosoftware.com>
2020-06-05 13:54:47 +0530

    removed login and eployee object, change the get method to return employee object.

--- leavetracker/src/LeaveTracker/EmployeeManager.cs
+++ leavetracker/src/LeaveTracker/EmployeeManager.cs
@@ -5,15 +5,13 @@ namespace LeaveTracker
 {
     public class EmployeeManager
     {
-        public Employee E { get; set; }
-
-        public bool GetEmployeeFromCsv(int id)
+        public static Employee GetEmployeeFromCsv(int id)
         {
+            var E = new Employee();
             using( var reader = File.OpenText("employee.csv"))
             {
                 var entry = reader.ReadLine(); //skip first line as it is header
-                entry = reader.ReadLine();
-                while(entry != null)
+                while(reader.ReadLine() != null)
                 {
                     var employ = entry.Split(",");
                     if (int.Parse(employ[0]) == id )
@@ -21,36 +19,11 @@ namespace LeaveTracker
                         E.ID = int.Parse(employ[0]);
                         E.Name = employ[1];
                         E.ManID = int.Parse(employ[2]);
-                        return true;
+                        break;
                     }
-                    entry = reader.ReadLine();
                 }
-                return false;
             }
-
-        }
-
-        public void AddEmployye()
-        {
-        }
-
-        public void login()
-        {
-            while(true)
-            {
-                Console.WriteLine("Enter the Employee ID or q to quit: ");
-                var input = Console.ReadLine();
-                if (input == "q")
-                {
-                    Environment.Exit(0);
-                }
-                var id = int.Parse(input);
-                if (GetEmployeeFromCsv(id))
-                {
-                    break;
-                }
-                Console.WriteLine("The ID is Invalid");
-            }
+            return E;
         }
     }
 }
